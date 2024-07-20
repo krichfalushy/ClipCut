e01d82fcffdaa76e997a1ac9fb00b45a1ac54329
dashock <bukashka0.00@gmail.com>
2024-07-20 14:56:50 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import tempfile
+import os
+import zipfile
+
+os.environ["IMAGEIO_FFMPEG_EXE"] = r"/opt/homebrew/bin/ffmpeg"
+
+from moviepy.editor import VideoFileClip, ipython_display, concatenate_videoclips, AudioFileClip
+
+
+# Function to cut the video on slices
+def split_video(path, num):
+    video_f = VideoFileClip(path)
+    clips = []
+    duration = video_f.duration / num
+
+    for i in range(num):
+        start = i * duration
+        end = (i + 1) * duration
+        clips.append(video_f.subclip(start, end))
+
+    return clips
+
+
+def generate_audio(clips, audio_path):
+    pass
+
+
+# STYLE CODE
+st.set_page_config(page_title="CLipCut", layout="wide")
+
+st.title(":orange[# Clip Cut]")
+st.write(" ")
+text = st.write(f'''
+#### Description
+bla bla lorem ipsum la la
+''')
+st.write("---")
+
+left_col, right_col = st.columns([1, 1])
+
+with st.container():
+
+    with left_col:
+        uploaded_video = st.file_uploader("###### Upload the video", type=["mp4", "mov", "avi", "mkv"])
+        clips_num = st.number_input("###### Number of clips to cut to", min_value=1, max_value=20, step=1)
+        clip_index = st.number_input("###### Number of clip to add the audio to", min_value=1, max_value=20, step=1)
+        columns_num = st.number_input("###### Number of columns for review", min_value=1, max_value=20, step=1)
+
+    with right_col:
+        st.write(" ")
+
+        # Uploaded video review
+        if uploaded_video:
+            _, container, _ = st.columns([15, 80, 15])
+            container.video(uploaded_video)
+
+
+with st.container():
+    st.write(" ")
+    st.write("### Write any theme for audio generation")
+    prompt = st.text_input(label="___Write a prompt, for example: 'flowers song'___")
+
+
+st.write("---")
+st.write(" ")
+st.write("### Your video clips")
+
+
+with st.container(height=500):
+    if uploaded_video and 1 <= clips_num and clip_index and columns_num <= 20 and prompt:
+        with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as f:
+            f.write(uploaded_video.read())
+            temp_file = f.name
+
+        video = VideoFileClip(temp_file)
+        temp_output = tempfile.mktemp(suffix=".mp4")
+        video.write_videofile(temp_output, codec="libx264", audio_codec="aac")
+
+        # List of clips
+        clips = split_video(temp_file, clips_num)
+        _, container, _ = st.columns([15, 80, 15])
+
+        # Creating temporary clip paths list
+        temp_paths = []
+        for i, clip in enumerate(clips):
+            temp_clip = os.path.join(tempfile.gettempdir(), f"clip_{i}.mp4")
+            clip.write_videofile(temp_clip, codec="libx264", audio_codec="aac")
+            temp_paths.append(temp_clip)
+
+        # Video by columns
+        # with st.expander("## Your video clips", expanded=True):
+        for i in range(0, len(temp_paths), columns_num):
+            cols = st.columns(columns_num)
+            for j, col in enumerate(cols):
+                if i + j < len(temp_paths):
+                    col.video(temp_paths[i + j])
+
+        # Download the archive
+        zip_path = os.path.join(tempfile.gettempdir(), "clips.zip")
+        with zipfile.ZipFile(zip_path, "w") as f:
+            for path in temp_paths:
+                f.write(path, arcname=os.path.basename(path))
+
+        with open(zip_path, "rb") as f:
+            st.download_button(label="Download the clips archive",
+                               data=f,
+                               file_name="clips.zip")
+
+
+
+
